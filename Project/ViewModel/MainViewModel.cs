eafa511b2c75bf4a57670c43340ce64e20cb1194
huynqhe153639 <huynqhe153639@fpt.edu.vn>
2023-02-28 21:05:00 +0700

    first commit

---
+++ Project/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Project.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+ 
+namespace Project.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public bool IsLoad = false;
+        public MainViewModel()
+        {
+            if(!IsLoad)
+            {
+                LoginWindown loginWindown = new LoginWindown();
+                loginWindown.Show();
+            }
+            IsLoad = true;
+
+
+        }
+    }
+}
